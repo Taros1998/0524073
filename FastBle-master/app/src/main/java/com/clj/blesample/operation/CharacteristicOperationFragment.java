c62d5ddb2ad8841ff81380f190de29b3814696f3
0524021 <0524021@nkust.edu.tw>
2019-01-02 14:29:48 +0800

    Update APP code

---
+++ FastBle-master/app/src/main/java/com/clj/blesample/operation/CharacteristicOperationFragment.java
@@ -0,0 +1,330 @@
+package com.clj.blesample.operation;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.clj.blesample.R;
+import com.clj.fastble.BleManager;
+import com.clj.fastble.callback.BleIndicateCallback;
+import com.clj.fastble.callback.BleNotifyCallback;
+import com.clj.fastble.callback.BleReadCallback;
+import com.clj.fastble.callback.BleWriteCallback;
+import com.clj.fastble.data.BleDevice;
+import com.clj.fastble.exception.BleException;
+import com.clj.fastble.utils.HexUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+public class CharacteristicOperationFragment extends Fragment{
+
+    public static final int PROPERTY_READ = 1;
+    public static final int PROPERTY_WRITE = 2;
+    public static final int PROPERTY_WRITE_NO_RESPONSE = 3;
+    public static final int PROPERTY_NOTIFY = 4;
+    public static final int PROPERTY_INDICATE = 5;
+    public static String hex = "";
+
+    private LinearLayout layout_container;
+    private List<String> childList = new ArrayList<>();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_characteric_operation, null);
+        initView(v);
+        return v;
+    }
+
+    private void initView(View v) {
+        layout_container = (LinearLayout) v.findViewById(R.id.layout_container);
+    }
+
+
+
+    @SuppressLint("ClickableViewAccessibility")
+    public void showData() {
+        final BleDevice bleDevice = ((OperationActivity) getActivity()).getBleDevice();
+        final BluetoothGattCharacteristic characteristic = ((OperationActivity) getActivity()).getCharacteristic();
+        final int charaProp = ((OperationActivity) getActivity()).getCharaProp();
+        String child = characteristic.getUuid().toString() + String.valueOf(charaProp);
+
+        for (int i = 0; i < layout_container.getChildCount(); i++) {
+            layout_container.getChildAt(i).setVisibility(View.GONE);
+        }
+        if (childList.contains(child)) {
+            layout_container.findViewWithTag(bleDevice.getKey() + characteristic.getUuid().toString() + charaProp).setVisibility(View.VISIBLE);
+        } else {
+            childList.add(child);
+
+            View view = LayoutInflater.from(getActivity()).inflate(R.layout.layout_characteric_operation, null);
+            view.setTag(bleDevice.getKey() + characteristic.getUuid().toString() + charaProp);
+
+
+
+            switch (charaProp) {
+
+                case PROPERTY_WRITE: {
+
+                    // go button test
+                    ImageButton go_btn = (ImageButton) view.findViewById(R.id.go_btn);
+                    ImageButton down_btn = (ImageButton) view.findViewById(R.id.down_btn);
+                    ImageButton left_btn = (ImageButton) view.findViewById(R.id.left_btn);
+                    ImageButton right_btn = (ImageButton) view.findViewById(R.id.right_btn);
+                    ImageButton back_right = (ImageButton) view.findViewById(R.id.back_right);
+                    ImageButton back_left = (ImageButton) view.findViewById(R.id.back_left);
+
+                    //hex = "";
+                    go_btn.setOnTouchListener(new View.OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                                hex = "67";
+                            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                                hex = "6E";
+                            }
+                            BleManager.getInstance().write(
+                                    bleDevice,
+                                    characteristic.getService().getUuid().toString(),
+                                    characteristic.getUuid().toString(),
+                                    HexUtil.hexStringToBytes(hex),
+                                    new BleWriteCallback() {
+
+                                        @Override
+                                        public void onWriteSuccess(final int current, final int total, final byte[] justWrite) {
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                }
+                                            });
+                                        }
+
+                                        @Override
+                                        public void onWriteFailure(final BleException exception) {
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                }
+                                            });
+                                        }
+                                    });
+                            return false;
+                        }
+                    });
+                    down_btn.setOnTouchListener(new View.OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                                hex = "62";
+                            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                                hex = "6E";
+                            }
+                            BleManager.getInstance().write(
+                                    bleDevice,
+                                    characteristic.getService().getUuid().toString(),
+                                    characteristic.getUuid().toString(),
+                                    HexUtil.hexStringToBytes(hex),
+                                    new BleWriteCallback() {
+
+                                        @Override
+                                        public void onWriteSuccess(final int current, final int total, final byte[] justWrite) {
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                }
+                                            });
+                                        }
+
+                                        @Override
+                                        public void onWriteFailure(final BleException exception) {
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                }
+                                            });
+                                        }
+                                    });
+                            return false;
+                        }
+                    });
+                    right_btn.setOnTouchListener(new View.OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                                hex = "72";
+                            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                                hex = "6E";
+                            }
+                            BleManager.getInstance().write(
+                                    bleDevice,
+                                    characteristic.getService().getUuid().toString(),
+                                    characteristic.getUuid().toString(),
+                                    HexUtil.hexStringToBytes(hex),
+                                    new BleWriteCallback() {
+
+                                        @Override
+                                        public void onWriteSuccess(final int current, final int total, final byte[] justWrite) {
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                }
+                                            });
+                                        }
+
+                                        @Override
+                                        public void onWriteFailure(final BleException exception) {
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                }
+                                            });
+                                        }
+                                    });
+
+                            return false;
+                        }
+                    });
+                    left_btn.setOnTouchListener(new View.OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                                hex = "6C";
+                            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                                hex = "6E";
+                            }
+                            BleManager.getInstance().write(
+                                    bleDevice,
+                                    characteristic.getService().getUuid().toString(),
+                                    characteristic.getUuid().toString(),
+                                    HexUtil.hexStringToBytes(hex),
+                                    new BleWriteCallback() {
+
+                                        @Override
+                                        public void onWriteSuccess(final int current, final int total, final byte[] justWrite) {
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                }
+                                            });
+                                        }
+
+                                        @Override
+                                        public void onWriteFailure(final BleException exception) {
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                }
+                                            });
+                                        }
+                                    });
+
+                            return false;
+                        }
+                    });
+                    back_right.setOnTouchListener(new View.OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                                hex = "70";
+                            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                                hex = "6E";
+                            }
+                            BleManager.getInstance().write(
+                                    bleDevice,
+                                    characteristic.getService().getUuid().toString(),
+                                    characteristic.getUuid().toString(),
+                                    HexUtil.hexStringToBytes(hex),
+                                    new BleWriteCallback() {
+
+                                        @Override
+                                        public void onWriteSuccess(final int current, final int total, final byte[] justWrite) {
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                }
+                                            });
+                                        }
+
+                                        @Override
+                                        public void onWriteFailure(final BleException exception) {
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                }
+                                            });
+                                        }
+                                    });
+
+                            return false;
+                        }
+                    });
+                    back_left.setOnTouchListener(new View.OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                                hex = "71";
+                            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                                hex = "6E";
+                            }
+                            BleManager.getInstance().write(
+                                    bleDevice,
+                                    characteristic.getService().getUuid().toString(),
+                                    characteristic.getUuid().toString(),
+                                    HexUtil.hexStringToBytes(hex),
+                                    new BleWriteCallback() {
+
+                                        @Override
+                                        public void onWriteSuccess(final int current, final int total, final byte[] justWrite) {
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                }
+                                            });
+                                        }
+
+                                        @Override
+                                        public void onWriteFailure(final BleException exception) {
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                }
+                                            });
+                                        }
+                                    });
+
+                            return false;
+                        }
+                    });
+                    //go button test
+                }
+                break;
+
+            }
+
+            layout_container.addView(view);
+        }
+    }
+
+    private void runOnUiThread(Runnable runnable) {
+        if (isAdded() && getActivity() != null)
+            getActivity().runOnUiThread(runnable);
+    }
+
+
+
+
+}
